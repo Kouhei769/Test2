971295a0cfe914e9463ddac11b2878a1492a119f
kouhei769 <2016216@s.asojuku.ac.jp>
2021-05-17 10:54:00 +0900

    矩形を動かす

--- Test/main.cpp
+++ Test/main.cpp
@@ -39,11 +39,46 @@ struct Rect
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	ChengeWindowMode(true);
+	ChangeWindowMode(true);
+	SetGraphMode(960, 540, 32);
 	DxLib_Init();
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	Rect rc = { 200, 200, 50, 100 };
 
+	char keystate[256];
+	constexpr float speed = 4.0f;
 
+
+	while (ProcessMessage() != -1)
+	{
+		ClearDrawScreen();
+		GetHitKeyStateAll(keystate);
+		if (keystate[KEY_INPUT_UP])
+		{
+			rc.center.y -= speed;
+		}
+
+		if (keystate[KEY_INPUT_DOWN])
+		{
+			rc.center.y += speed;
+
+		}
+
+		if (keystate[KEY_INPUT_RIGHT])
+		{
+			rc.center.x += speed;
+		}
+
+		if (keystate[KEY_INPUT_LEFT])
+		{
+			rc.center.x -= speed;
+		}
+
+
+		DrawBox(rc.Left(), rc.Top(), rc.Right(), rc.bottom(), 0xfffff, true);
+		ScreenFlip();
+	}
+
+	DxLib_End();
 }
